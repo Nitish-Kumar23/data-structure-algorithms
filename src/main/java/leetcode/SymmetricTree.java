cf96568b980fdfe4a3cf47b4ae1d706dfe23f1b8
Nitish-Kumar23 <54999521+Nitish-Kumar23@users.noreply.github.com>
2024-01-18 03:07:40 +0530

    lc-101 | symmetric tree - bfs

--- src/main/java/leetcode/SymmetricTree.java
+++ src/main/java/leetcode/SymmetricTree.java
@@ -2,6 +2,9 @@ package leetcode;
 
 import baseclass.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SymmetricTree {
 
     public boolean isSymmetric(TreeNode root) {
@@ -22,6 +25,34 @@ public class SymmetricTree {
             return false;
         }
         return isSymmetric(node1.left,node2.right) && isSymmetric(node2.left,node1.right);
+    }
+
+    public boolean isSymmetricQueue(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (!queue.isEmpty()){
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if(node1==null && node2==null){
+                continue;
+            }
+            if(node1==null || node2==null){
+                return false;
+            }
+            if(node1.val!=node2.val){
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.right);
+
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
     }
 
 }
