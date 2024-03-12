0afbb24fbcceb342b6dc01f8655bea4f8a7304b8
Nitish-Kumar23 <54999521+Nitish-Kumar23@users.noreply.github.com>
2024-03-13 02:37:40 +0530

    lc-106 | construct binary tree from inorder & postorder traversal

---
+++ src/main/java/leetcode/BinaryTreeFromInAndPostOrderTraversal.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import baseclass.TreeNode;
+
+import java.util.Arrays;
+
+public class BinaryTreeFromInAndPostOrderTraversal {
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(postorder.length==0){
+            return null;
+        }
+
+        int rootVal = postorder[postorder.length-1];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = 0;
+        for (int i=0;i<inorder.length;i++){
+            if(inorder[i]==rootVal){
+                index = i;
+                break;
+            }
+        }
+
+        root.left = buildTree(Arrays.copyOfRange(inorder,0,index),Arrays.copyOfRange(postorder,0,index));
+        root.right = buildTree(Arrays.copyOfRange(inorder,index+1,inorder.length),Arrays.copyOfRange(postorder,index,postorder.length-1));
+
+        return root;
+    }
+
+}
