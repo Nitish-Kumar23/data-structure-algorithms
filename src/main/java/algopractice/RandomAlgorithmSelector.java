733d6539fc5fc04f1b99e1305358e05728028db9
Nitish-Kumar23 <54999521+Nitish-Kumar23@users.noreply.github.com>
2024-09-23 00:26:15 +0530

    random algorithm selector

---
+++ src/main/java/algopractice/RandomAlgorithmSelector.java
@@ -0,0 +1,32 @@
+package algopractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class RandomAlgorithmSelector {
+
+    private static final List<String> algorithms = Arrays.asList("BINARY_SEARCH","SELECTION_SORT","QUICK_SORT","MERGE_SORT","BST_IMPLEMENTATION");
+
+    public static final Random random = new Random();
+    public static void main(String[] args) {
+        System.out.println(randomAlgorithm());
+    }
+
+    private static List<String> randomAlgorithm() {
+        List<String> result = new ArrayList<>();
+        if(algorithms.isEmpty()){
+            return result;
+        }
+
+        result.add(algorithms.get(getRandomIndex(0,algorithms.size())));
+        result.add(algorithms.get(getRandomIndex(0,algorithms.size())));
+        return  result;
+    }
+
+    private static Integer getRandomIndex(Integer minBound, Integer maxBound) {
+        return random.nextInt(minBound,maxBound);
+    }
+
+}
