6cf9e7bcb4ce4c76682ce2a6721da2714f71feb5
Nitish-Kumar23 <54999521+Nitish-Kumar23@users.noreply.github.com>
2025-04-15 01:07:47 +0530

    has duplicate

--- src/main/java/NeetCode/HasDuplicate.java
+++ src/main/java/NeetCode/HasDuplicate.java
@@ -1,8 +1,11 @@
 package NeetCode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+
+// https://neetcode.io/problems/duplicate-integer
 class HasDuplicate {
     public boolean hasDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -12,6 +15,20 @@ class HasDuplicate {
             }
             set.add(val);
         }
+        return false;
+    }
+
+    public boolean hasDuplicateV1(int[] nums) {
+        if(nums.length==0 || nums.length==1){
+            return false;
+        }
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+
         return false;
     }
 }
